59de88f45415b2278366a50abfd28985a89073a8
mateki0 <mateki0@interia.pl>
2022-03-19 14:08:26 +0100

    fixed photo placeholder svg

--- lib/widgets/userPhoto.dart
+++ lib/widgets/userPhoto.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 Widget UserPhoto() {
-  const String assetName = 'assets/photo-placeholder.svg';
+  const String assetName = 'assets/photo.svg';
 
   return (SizedBox(
       width: 64,
       height: 64,
       child: Container(
+        // margin: const EdgeInsets.only(top: 5),
+        alignment: const Alignment(0.0, 1.0),
         child: SvgPicture.asset(assetName),
+        clipBehavior: Clip.hardEdge,
         decoration: BoxDecoration(
             color: const Color(0xffE9EAEE),
             borderRadius: BorderRadius.circular(32.0)),
